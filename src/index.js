47d56fa83f0dccf72d2522ec67878b24f49640e9
Isaac FG <i.feria.g952@edu.utc.mx>
2025-06-25 07:54:04 -0600

    inicio de proyecto

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// Middleware para procesar JSON
+app.use(express.json());
+
+// Configuración del puerto y formato JSON
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+// Ruta principal - GET /
+app.get('/', (req, res) => {
+    res.json({
+        Title: 'Hola mundo'
+    });
+});
+app.get('/hola', (req, res) => {
+    res.json({
+        Title: 'Cambios'
+    });
+});
+
+// Ruta mensaje - GET /mensaje
+app.get('/mensaje', (req, res) => {
+    res.json({
+        Title: 'Este es mi nuevo servidor'
+    });
+});
+
+// Ruta para sumar dos números - POST /sumar
+app.post('/sumar', (req, res) => {
+    const { num1, num2 } = req.body;
+
+    // Validación: asegurarse de que ambos números estén presentes
+    if (num1 == null || num2 == null) {
+        return res.status(400).json({ error: 'Faltan dos números para sumar' });
+    }
+
+    // Realizar la suma
+    const resultado = num1 + num2;
+
+    // Enviar resultado como JSON
+    res.json({ resultado });
+});
+
+// Iniciar el servidor
+app.listen(app.get('port'), () => {
+    console.log(`Servidor escuchando en http://localhost:${app.get('port')}`);
+});
